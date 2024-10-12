f7590508e89f58df7dd6c7ba0fb1ec21ef303587
Toriomg <150125285+Toriomg@users.noreply.github.com>
2024-10-12 19:53:40 +0200

    Coment network.py
    simplemente lo he comentado la función ppal. y le he añadido una linea de códgio que creo q faltaba al hacer los services

--- src/network.py
+++ src/network.py
@@ -4,6 +4,7 @@ import random
 line_no = 0
 def read_network():
     class Kind(enum.Enum):
+        #Como el input está dividido por partes, enum ayuda a dividirlo
         SIZE = 0
         NODES = 1
         EDGES = 2
@@ -11,37 +12,44 @@ def read_network():
         SERVICES = 4
         END = 5
 
-    global line_no
+    global line_no # Para contar las lineas del input que se pasan
     kind = Kind.SIZE
     nodes = []
-    edges_read = 0
+    edges_read = 0 #num de lineas de edges leídas
     edges = []
-    services_read = 0
+    services_read = 0 #num de lineas de services leídas
     services = []
 
     while kind != Kind.END:
-        line_no += 1
+        # Set the network (graph) properties during iterations
+        line_no += 1 # Add a read line
         l = input()
         print(l)
         if kind == Kind.SIZE:
-            kind = Kind.NODES
+            #Set the size of the network (graph)
+            kind = Kind.NODES # Move to nodes
             nodes_no, edges_no = [int(x) for x in l.split()]
         elif kind == Kind.NODES:
-            kind = Kind.EDGES
+            #Set the list of nodes
+            kind = Kind.EDGES # Move to edges
             nodes.append(tuple(int(x) for x in l.split()))
         elif kind == Kind.EDGES:
+            # For each iteration, add a edge to the network (graph)
             edges_read += 1
             if edges_read == edges_no:
                 kind = Kind.SERVICES_NO
             edges.append(tuple(int(x) for x in l.split()))
         elif kind == Kind.SERVICES_NO:
+            #Set the number of services in the network
             kind = Kind.SERVICES
             services_no = int(l)
         elif kind == Kind.SERVICES:
+            # Add a service to the network (graph)
             if services_read == services_no:
                 kind = Kind.END
             services_read += 1
             service = tuple(int(x) for x in l.split())
+            line_no += 1 # Add a read line(he añadido esto, que antes no estaba, para q no cuente menos líneas)
             l = input()
             service_edges = tuple(int(x) for x in l.split())
             services.append((service, service_edges))
