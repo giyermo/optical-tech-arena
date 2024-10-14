2b56f8ce034c251a7ed85ed8331eb5830e0c7e15
giyermo <guigonav@gmail.com>
2024-10-14 17:17:09 +0200

    removed print statement

--- src/main.py
+++ src/main.py
@@ -10,7 +10,6 @@ base_network = Network(nodes, edges, services)
 
 scenarios = produce_scenarios(edges)
 print_scenarios(scenarios)
-print(base_network.nodes)
 
 scenarios_no = int(input()) # Number of scenarios provided by the environment
 
