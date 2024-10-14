182cba098a4c84bfa0865ffa03c987a20dbd3a0e
Toriomg <150125285+Toriomg@users.noreply.github.com>
2024-10-14 02:01:16 +0200

    SON LAS 2am QUE HAGO CON MI VIDA
    solo he añadido lo q se me ha ocurrido

--- src/network.py
+++ src/network.py
@@ -21,3 +21,43 @@ class Network:
 
     def solve_scenario(self, base_network):
         print("0", flush=True)
+
+    def dijkstra(self, start_node, end_node) -> list:
+        distances = {node: float('inf') for node in self.nodes} #Init all nodes in a dict with infinty distance
+        distances[start_node] = 0
+        priority_queue = [(0, start_node)]
+        # A SLinked list would be better
+        predecessors = {node: None for node in self.nodes} # The value is just the next key, so you assure the path
+
+        while priority_queue:
+            current_distance, current_node = heapq.heappop(priority_queue) #pop the queue
+
+            if current_node == end_node: #reached end node
+                #Found end_node and create the path list
+                path = []
+                while current_node is not None:
+                    path.append(current_node)
+                    current_node = predecessors[current_node] 
+                return path[::-1] # Reversed, then it will be from start to end
+            if current_distance > distances[current_node]:
+                continue
+
+            for neighbor in self.edges[current_node]:"""EL COMO FUNCIONA SELF.EDGES NO HACE Q VAYA COMO DEBERIA :("""
+                distance = current_distance + 1 # The weight of an edge is 1
+                if distance < distances[neighbor]:
+                        distances[neighbor] = distance
+                        predecessors[neighbor] = current_node
+                        heapq.heappush(priority_queue, (distance, neighbor)) #push the queue
+        return None #No path is found, which should be an error as the graph should be connected        
+    
+    def solve_scenatio_hmg_beta_1(self):
+        #i'll assume the algorythm is executed after a failure occurs in a node
+        self.sort_services(self.services)
+        #Assuming my proposition on self.services
+        broken_services = [] #Aassuming a segregation of the services that fail
+        for serv in broken_services:
+            new_serv = copy.deepcopy(serv)
+            #As the edge is deleted find the sortest path
+            new_path = self.dijkstra(serv['src'],serv['dst'])
+            """ESTO NO SE PUEDE PQ DEVUELVE NODOS Y NO LOS INDICES DE VERTICES :("""
+            new_serv['path'] = new_path
