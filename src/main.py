a76ce67e1a2de5175532e3d312bd4e4baeb05082
giyermo <guigonav@gmail.com>
2024-10-15 20:54:09 +0200

    new possible submission

--- src/main.py
+++ src/main.py
@@ -14,18 +14,51 @@ print_scenarios(scenarios)
 scenarios_no = int(input()) # Number of scenarios provided by the environment
 
 for _ in range(scenarios_no): # For each scenario
-    
     edges = [int(x) for x in input().split()] #parse edges that fail
     scenario = copy.deepcopy(base_network) # copy base network
+    broken_serv = []
+    fixed_serv = []
 
     while edges[0] != -1:
         for edge in edges: # Delete edges that fail
             scenario.deactivate_edge(edge)
             if scenario.edges[edge]["services"] != []:
                 for service in scenario.edges[edge]["services"]:
-                    scenario.deactivate_service(service-1)
+                    if scenario.services[service-1]["dead"] == False:   
+                        broken_serv.append(service)
+                    scenario.deactivate_service(service)
 
-        print(scenario.min_dist_path(1, 5))
-        scenario.solve_scenario(base_network)
+
+        for idx in broken_serv:
+            service = scenario.services[idx-1]
+            if service["dead"] == False:
+                redirect = scenario.redirect_broken_service(service)
+                if redirect == None:
+                    if idx not in broken_serv:
+                        broken_serv.append(idx)
+                    if idx in fixed_serv:
+                        fixed_serv.remove(idx)
+                else:
+                    if idx not in fixed_serv:
+                        fixed_serv.append(idx)
+                    if idx in broken_serv:
+                        broken_serv.remove(idx)
         
+        print(len(fixed_serv), flush=True)
+
+        for service in fixed_serv:
+            print(service, len(scenario.services[service-1]["path"]), flush=True)
+
+            output = []
+
+            for edge in scenario.services[service-1]["path"]:
+                start = scenario.services[service-1]["wavelengths"][0]
+                end = scenario.services[service-1]["wavelengths"][1]
+                output.extend([edge, start, end])
+            print(*output, flush=True)
+
+
+        broken_serv = []
+        fixed_serv = []
+
         edges = [int(x) for x in input().split()] # set next batch of edges
